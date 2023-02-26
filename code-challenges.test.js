cf99935f32b88695086c686274697f8592ce8568
BxRay95 <bray387@outlook.com>
2023-02-26 09:59:03 -0800

    added the answers to the assessment for week 2

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,33 +21,121 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // b) Create the function that makes the test pass.
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------1) Create a function that takes an object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+
+
+// psuedocode: 
+// create a function called divisibleBy3
+// input: taking in a random number
+// determine if that number is divisible by 3 using an else if statement
+// output: return the statement in a string whether the number is divisible by 3 or not, with a catch all that returns an error if the given input is not a number
+
+
+describe("divisibleBy3", () => {
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
 const object2 = { number: 0 }
 // Expected output: "0 is divisible by three"
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
+    it("returns whether or not the number is divisible by 3", () => {
+      expect(divisibleBy(object1)).toEqual("15 is divisible by 3")
+      expect(divisibleBy(object2)).toEqual("0 is divisible by 3")
+      expect(divisibleBy(object3)).toEqual("-7 is not divisible by 3")
+    })
+})
+// ---> divisibleBy3 is not defined
 
-// b) Create the function that makes the test pass.
+const divisibleBy = (object) => {
+    if(object.number % 3 === 0){
+        return `${object.number} is divisible by 3`
+    } else {
+        return `${object.number} is not divisible by 3`
+    }
+}
+// output ---> pass
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+
+describe("capitalLetters", () => {
+it("returns an array with all the words capitalized", () => {
+    const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+    expect(capitalLetters(["streetlamp", "potato", "teeth", "conclusion", "nephew"])).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+    expect(capitalLetters(["temperature", "database", "chopsticks", "mango"])).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+  })
+})
+// output ---> capitalLetters is not defined
 
 // b) Create the function that makes the test pass.
+
+// psuedocode:
+// input: an array of various words
+// using the built in method .map, iterate through the array and using charAt pinpoint the first character of each word and .toUpperCase to make each of them capitalized
+// output: will return an array with all of the first letters of the words capitalized 
+
+const capitalLetters = (array) => {
+    return array.map((value) => {
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    })
+}
+
+
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("vowelExtractor", () => {
+    const vowelTester1 = "learn"
+// Expected output: 1
+const vowelTester2 = "academy"
+// Expected output: 0
+const vowelTester3 = "challenges"
+// Expected output: 2
+it("returns the index of the first vowel", () => {
+    expect(vowelExtractor(vowelTester1)).toEqual(1)
+    expect(vowelExtractor(vowelTester2)).toEqual(0)
+    expect(vowelExtractor(vowelTester3)).toEqual(2)
+  })
+})
+
+// Pseudocode: 
+// input: a string
+// create a function called vowelExtractor that takes in a string as a parameter. Create the vowelsArray, split the input string into an array. Use forEach to iterate through each character(item) to use in the conditional. In the conditional its checking for the index using the built in method indexOf to find the vowel if the index of it does not equal -1, meaning it does exist within the string and meets the other condition of being undefined since vowelsIndex is not assigned until after the conditions are both met. When both conditions are met vowelsIndex is assigned to the index of the vowel appearing in the given input
+// output: will return the index position of the first vowel in the given string
+
+// b) Create the function that makes the test pass.
+
+
+const vowelsArray = ["a", "e", "i", "o", "u", "y"]
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -56,4 +144,12 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
-// b) Create the function that makes the test pass.
+function vowelExtractor(string) {
+    let vowelIndex
+    string.split("").forEach(item => {
+        if(vowelsArray.indexOf(item) != -1 && vowelIndex === undefined) {
+            vowelIndex = string.indexOf(item) 
+         }
+    });
+    return vowelIndex
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: so a paramenter is the scope of your function essentially, and the argument is what you are using in your function to compare against? if thats a good way of putting it, thats how i remember it at least
 
-Researched answer:
+Researched answer: the best answer i could find to explain this, and helped me understand it better was that the parameters are the aliases for the values inside the function and the arguments are the actual values.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: so I am VERY FAMILIAR with this after spending abvout 5 hours on the third question of the code challenge questions........... 
+you need a function, im not sure if that is considered a parameter though... element is the first which is required, index is the second and is optional, and the last is array which is also optional.
 
-Researched answer:
+Researched answer: so I had all of them essentially correct but there is another optional thisValue that I assume it used with objects? im having trouble finding specific instances of this, so i assume it use scarcely used.
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: from what i remember, .map iterates through an array and returns an array of the same length. and .filter only returns values that meet a specified condition, so not necessarily of the same length in return.
 
-Researched answer:
+Researched answer: i was spot on on this one, map returns an array that has the same length as the original and filter removes the elements that dont belong after the conditions are met
 
-1. What is iteration?
+4. What is iteration?
 
-Your answer:
+Your answer: in simple ways of thinking iteration is acting on a function over the values within the function. 
 
-Researched answer:
+Researched answer: performing an action however many times is needed in order to meet the condition specified within the function
 
-1. What is the difference between a function and a method?
+5. What is the difference between a function and a method?
 
-Your answer:
+Your answer: functions are written code to perform actions, and methods are held within functions as objects to be called on.
 
-Researched answer:
+Researched answer: i was actually pretty close with this one, methods are still functions they just exist inside of functions as properties
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: i cant even going to pretend like i know what this means, i honestly dont even have a best guess. something with lifting? like making something easier to access in some way?
 
-Researched answer:
+Researched answer: im finding its essentially javascripts defualt way of moving declarations to the top for access.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Classes:
+1. Classes: classes look like more in depth functions that have a different scope... amazing, i barely understand the scope of old functions..
 
-2. Spread operator:
+2. Spread operator: it lets us see the parameters within an array? im not sure im not finding a lot of good examples on this.
 
-3. React state:
+3. React state: data storage variable that chooses how a value acts.
 
-4. React props:
+4. React props: properties, allow the passing of data between components
 
-5. DOM events:
+5. DOM events: a signal that lets you know something is happening? this one is confusing and hard to find an actual definition for.
